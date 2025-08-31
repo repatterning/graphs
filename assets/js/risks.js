2238b89b8f6372a6f2008176b410f87de765d563
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-08-31 22:20:13 +0100

    Update assets/js/risks.js

--- assets/js/risks.js
+++ assets/js/risks.js
@@ -1,157 +1,193 @@
 var Highcharts;
+var optionSelected;
+var dropdown = $('#option_selector');
+var url = '../warehouse/risks/menu/menu.json';
 
 
-jQuery.getJSON('../warehouse/risks/points/0001.json', function (source)  {
+$.getJSON(url, function (data) {
 
-    // https://api.highcharts.com/highstock/plotOptions.series.dataLabels
-    // https://api.highcharts.com/class-reference/Highcharts.Point#.name
-    // https://api.highcharts.com/highstock/tooltip.pointFormat
+    $.each(data, function (key, entry) {
+        dropdown.append($('<option></option>').attr('value', entry.desc).text(entry.name));
+    });
 
+    // Load the first Option by default
+    var defaultOption = dropdown.find("option:first-child").val();
+    optionSelected = dropdown.find("option:first-child").text();
 
-    let estimates = [];
+    // Generate
+    generateChart(defaultOption);
 
-
-    // split the data set into ...
-    for (let i = 0; i < source.length; i += 1) {
-
-        let indices = source[i]['columns'];
-        let i_max = indices.indexOf('maximum'),
-            i_lat = indices.indexOf('latest'),
-            i_med = indices.indexOf('median'),
-            i_sta_n = indices.indexOf('station_name'),
-            i_riv = indices.indexOf('river_name');
+});
 
 
-        let data = [];
-        for (let j = 0; j < source[i]['data'].length; j += 1) {
+// Dropdown
+dropdown.on('change', function (e) {
 
-            data.push({
-                x: source[i]['data'][j][i_lat], // maximum
-                y: source[i]['data'][j][i_max], // latest
-                name: source[i]['data'][j][i_sta_n] // station name
-                // description: 'River: ' + source[i]['data'][j][i_riv] + ', Median: ' + source[i]['data'][j][i_med] + ' mm/hr'
+    $('#option_selector_title').remove();
+
+    // Save name and value of the selected option
+    optionSelected = this.options[e.target.selectedIndex].text;
+    var valueSelected = this.options[e.target.selectedIndex].value;
+
+    //Draw the Chart
+    generateChart(valueSelected);
+});
+
+
+// Generate graphs
+function generateChart(fileNameKey) {
+
+    $.getJSON('../warehouse/risks/points/' + fileNameKey + '.json', function (source) {
+
+        // https://api.highcharts.com/highstock/plotOptions.series.dataLabels
+        // https://api.highcharts.com/class-reference/Highcharts.Point#.name
+        // https://api.highcharts.com/highstock/tooltip.pointFormat
+
+
+        let estimates = [];
+
+
+        // split the data set into ...
+        for (let i = 0; i < source.length; i += 1) {
+
+            let indices = source[i]['columns'];
+            let i_max = indices.indexOf('maximum'),
+                i_lat = indices.indexOf('latest'),
+                i_med = indices.indexOf('median'),
+                i_sta_n = indices.indexOf('station_name'),
+                i_riv = indices.indexOf('river_name');
+
+
+            let data = [];
+            for (let j = 0; j < source[i]['data'].length; j += 1) {
+
+                data.push({
+                    x: source[i]['data'][j][i_lat], // maximum
+                    y: source[i]['data'][j][i_max], // latest
+                    name: source[i]['data'][j][i_sta_n] // station name
+                    // description: 'River: ' + source[i]['data'][j][i_riv] + ', Median: ' + source[i]['data'][j][i_med] + ' mm/hr'
+                });
+
+            }
+
+            estimates.push({
+                type: 'scatter',
+                name: source[i]['catchment_name'],
+                data: data,
+                className: source[i]['catchment_name'], // for point classification by catchment
+                tooltip: {
+                    pointFormat: '<span style="color:{point.color}">\u25CF</span> <b> {series.name} </b>:<br/>' +
+                        'name: {point.name}<br/> maximum: {point.y:,.3f}<br/>latest: {point.x:,.3f}<br/>'
+                }
             });
 
         }
 
-        estimates.push({
-            type: 'scatter',
-            name: source[i]['catchment_name'],
-            data: data,
-            className: source[i]['catchment_name'], // for point classification by catchment
-            tooltip: {
-                pointFormat: '<span style="color:{point.color}">\u25CF</span> <b> {series.name} </b>:<br/>' +
-                    'name: {point.name}<br/> maximum: {point.y:,.3f}<br/>latest: {point.x:,.3f}<br/>'
+
+        Highcharts.setOptions({
+            lang: {
+                thousandsSep: ','
             }
         });
 
-    }
 
+        // Draw a graph
+        Highcharts.chart('container0002', {
 
-    Highcharts.setOptions({
-        lang: {
-            thousandsSep: ','
-        }
-    });
-
-
-    // Draw a graph
-    Highcharts.chart('container0002', {
-
-        chart: {
-            type: 'scatter',
-            zoomType: 'xy'
-        },
-
-        title: {
-            text: 'Rates of Change of River Levels'
-        },
-
-        subtitle: {
-            text: '<p>vis-à-vis Scotland\'s river level gauge station measures.</p> <br/><br/>'
-        },
-
-        time: {
-            timezone: 'Europe/London'
-        },
-
-        credits: {
-            enabled: false
-        },
-
-        legend: {
-            enabled: true
-        },
-
-        exporting: {
-            buttons: {
-                contextButton: {
-                    menuItems: [ 'viewFullscreen', 'printChart', 'separator',
-                        'downloadPNG', 'downloadJPEG', 'downloadPDF', 'downloadSVG' , 'separator',
-                        'downloadXLS', 'downloadCSV']
-                }
-            }
-        },
-
-        xAxis: {
-            title: {
-                text: 'latest (mm/hr)'
+            chart: {
+                type: 'scatter',
+                zoomType: 'xy'
             },
-            labels: {
-                format: '{value}'
-            }
-        },
 
-        yAxis: {
-            labels: {
-                format: '{value}'
-            },
             title: {
-                text: 'maximum (mm/hr)',
-                x: 0
-            }
-        },
+                text: 'Rates of Change of River Levels'
+            },
 
-        plotOptions:{
-            series: {
-                turboThreshold: 4000
-            }
-        },
+            subtitle: {
+                text: '<p>vis-à-vis Scotland\'s river level gauge station measures.</p> <br/><br/>'
+            },
 
-        tooltip: {
-            split: true,
-            dateTimeLabelFormats: {
-                millisecond:"%A, %e %b, %H:%M:%S.%L",
-                second:"%A, %e %b, %H:%M:%S",
-                minute:"%A, %e %b, %H:%M",
-                hour:"%A, %e %b, %H:%M",
-                day:"%A, %e %B, %Y",
-                week:"%A, %e %b, %Y",
-                month:"%B %Y",
-                year:"%Y"
-            }
+            time: {
+                timezone: 'Europe/London'
+            },
 
-        },
+            credits: {
+                enabled: false
+            },
 
-        series: estimates,
-        responsive: {
-            rules: [{
-                condition: {
-                    maxWidth: 700
-                },
-                chartOptions: {
-                    rangeSelector: {
-                        inputEnabled: false
+            legend: {
+                enabled: true
+            },
+
+            exporting: {
+                buttons: {
+                    contextButton: {
+                        menuItems: ['viewFullscreen', 'printChart', 'separator',
+                            'downloadPNG', 'downloadJPEG', 'downloadPDF', 'downloadSVG', 'separator',
+                            'downloadXLS', 'downloadCSV']
                     }
                 }
-            }]
-        }
+            },
+
+            xAxis: {
+                title: {
+                    text: 'latest (mm/hr)'
+                },
+                labels: {
+                    format: '{value}'
+                }
+            },
+
+            yAxis: {
+                labels: {
+                    format: '{value}'
+                },
+                title: {
+                    text: 'maximum (mm/hr)',
+                    x: 0
+                }
+            },
+
+            plotOptions: {
+                series: {
+                    turboThreshold: 4000
+                }
+            },
+
+            tooltip: {
+                split: true,
+                dateTimeLabelFormats: {
+                    millisecond: "%A, %e %b, %H:%M:%S.%L",
+                    second: "%A, %e %b, %H:%M:%S",
+                    minute: "%A, %e %b, %H:%M",
+                    hour: "%A, %e %b, %H:%M",
+                    day: "%A, %e %B, %Y",
+                    week: "%A, %e %b, %Y",
+                    month: "%B %Y",
+                    year: "%Y"
+                }
+
+            },
+
+            series: estimates,
+            responsive: {
+                rules: [{
+                    condition: {
+                        maxWidth: 700
+                    },
+                    chartOptions: {
+                        rangeSelector: {
+                            inputEnabled: false
+                        }
+                    }
+                }]
+            }
+        });
+
+    }).fail(function () {
+        console.log("Missing");
+        $('#container0002').empty();
     });
 
-}).fail(function() {
-    console.log("Missing");
-    $('#container0002').empty();
-});
 
-
-
+}
