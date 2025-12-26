ce71ef25f107127e6b06d1b04234e5b12098307a
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-12-26 22:39:23 +0000

    Update assets/js/inspect/arc-rnn-lstm-estimates.js

--- assets/js/inspect/arc-rnn-lstm-estimates.js
+++ assets/js/inspect/arc-rnn-lstm-estimates.js
@@ -1,7 +1,7 @@
 var Highcharts;
 var optionSelected;
 var dropdown = $('#option_selector');
-var url = '../warehouse-t/arc-rnn-lstm-metrics/disaggregates/menu/menu.json';
+var url = '../../../warehouse-t/arc-rnn-lstm-metrics/disaggregates/menu/menu.json';
 
 
 $.getJSON(url, function (data) {
@@ -37,7 +37,7 @@ dropdown.on('change', function (e) {
 // Generate graphs
 function generateChart(fileNameKey) {
 
-    $.getJSON('../warehouse-t/arc-rnn-lstm-metrics/disaggregates/points/' + fileNameKey + '.json', function (source) {
+    $.getJSON('../../../warehouse-t/arc-rnn-lstm-metrics/disaggregates/points/' + fileNameKey + '.json', function (source) {
 
         // https://api.highcharts.com/highstock/plotOptions.series.dataLabels
         // https://api.highcharts.com/class-reference/Highcharts.Point#.name
