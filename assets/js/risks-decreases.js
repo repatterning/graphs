7b5c7221cb382bd98def5218c9ba04687640818c
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2026-01-11 22:13:58 +0000

    Update assets/js/risks-decreases.js

---
+++ assets/js/risks-decreases.js
@@ -0,0 +1,179 @@
+var Highcharts;
+var optionSelected;
+var dropdown = $('#option_selector');
+var url = '../warehouse/risks/menu/menu.json';
+
+
+$.getJSON(url, function (data) {
+
+    $.each(data, function (key, entry) {
+        dropdown.append($('<option></option>').attr('value', entry.desc).text(entry.name));
+    });
+
+    // Load the first Option by default
+    var defaultOption = dropdown.find("option:first-child").val();
+    optionSelected = dropdown.find("option:first-child").text();
+
+    // Generate
+    generateChart(defaultOption);
+
+});
+
+
+// Dropdown
+dropdown.on('change', function (e) {
+
+    // $('#option_selector_title').remove();
+
+    // Save name and value of the selected option
+    optionSelected = this.options[e.target.selectedIndex].text;
+    var valueSelected = this.options[e.target.selectedIndex].value;
+
+    //Draw the Chart
+    generateChart(valueSelected);
+});
+
+
+// Generate graphs
+function generateChart(fileNameKey) {
+
+    $.getJSON('../warehouse/risks/points/' + fileNameKey + '.json', function (source) {
+
+        // https://api.highcharts.com/highstock/plotOptions.series.dataLabels
+        // https://api.highcharts.com/class-reference/Highcharts.Point#.name
+        // https://api.highcharts.com/highstock/tooltip.pointFormat
+
+
+        // The fields
+        const columns = {
+            station: [],
+            latest: [],
+            maximum: [],
+            catchment: []
+            // coordinates: []
+        };
+
+
+        // Data
+        for (let i = 0; i < source.length; i += 1) {
+
+            let indices = source[i]['columns'];
+            let i_latest = indices.indexOf('latest'),
+                i_maximum = indices.indexOf('maximum'),
+                i_station = indices.indexOf('station_name');
+            // i_latitude = indices.indexOf('latitude'),
+            // i_longitude = indices.indexOf('longitude');
+
+            for (let j = 0; j < source[i]['data'].length; j += 1) {
+
+                if (source[i]['data'][j][i_latest] >= 0) {
+                    continue;
+                }
+
+                columns.station.push(source[i]['data'][j][i_station]);
+                columns.latest.push(Highcharts.numberFormat(source[i]['data'][j][i_latest], 4));
+                columns.maximum.push(Highcharts.numberFormat(source[i]['data'][j][i_maximum], 4))
+                columns.catchment.push(source[i]['catchment_name']);
+
+                // let point = [source[i]['data'][j][i_latitude], source[i]['data'][j][i_longitude]];
+                // columns.coordinates.push(point);
+
+            }
+
+        }
+
+
+        // Grid
+        Grid.grid('container0003', {
+
+            dataTable: {
+                columns: columns
+            },
+
+            lang: {
+                pagination: {
+                    pageInfo: `{start} - {end} of {total}
+                (page {currentPage} of {totalPages})`,
+                    rowsPerPage: 'rows per page'
+                }
+            },
+
+            credits: {
+                enabled: false
+            },
+
+            rendering: {
+                rows: {
+                    minVisibleRows: 10
+                }
+            },
+
+            pagination: {
+                enabled: true,
+                pageSize: 10,
+                controls: {
+                    pageSizeSelector: {
+                        enabled: true,
+                        options: [10, 20, 50]
+                    },
+                    pageInfo: true,
+                    firstLastButtons: true,
+                    previousNextButtons: true,
+                    pageButtons: {
+                        enabled: true,
+                        count: 7
+                    }
+                }
+            },
+
+            // https://api.highcharts.com/grid/#interfaces/Grid_Core_Options.Options#columndefaults
+            columnDefaults: {
+                sorting: {
+                    sortable: true
+                }
+            },
+
+            // https://api.highcharts.com/grid/#interfaces/Grid_Core_Options.Options#columns
+            columns: [{
+                id: 'latest',
+                width: 115,
+                header: {
+                    format: '<b>Latest</b><br>(mm/hr)'
+                },
+                sorting: {
+                    enabled: true,
+                    order: 'desc'
+                }
+            }, {
+                id: 'maximum',
+                width: 115,
+                header: {
+                    format: '<b>Maximum</b><br>(mm/hr)'
+                }
+            },{
+                id: 'catchment',
+                width: 205,
+                header: {
+                    format: '<b>Catchment</b>'
+                },
+                filtering: {
+                    enabled: true,
+                    inline: false,
+                    condition: 'contains'
+                }
+            }, {
+                id: 'station',
+                width: 165,
+                header: {
+                    format: '<b>Station</b>'
+                }
+            }]
+
+        });
+
+    }).fail(function () {
+        console.log("Missing");
+        $('#container0003').empty();
+    });
+
+}
