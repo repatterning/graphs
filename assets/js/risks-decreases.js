127ac8264d6dd1736b7733d3f53c6094e3f191d8
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2026-01-12 23:09:23 +0000

    Update assets/js/risks-decreases.js

--- assets/js/risks-decreases.js
+++ assets/js/risks-decreases.js
@@ -77,7 +77,7 @@ function generateChart(fileNameKey) {
                     name = source[i]['data'][j][i_station];
 
                 // Street Images
-                let point = `<a href='https://www.openstreetmap.org/?mlat=${latitude}&mlon=${longitude}&zoom=12#map=16/${latitude}/${longitude}' class='btn btn-sm btn-outline-primary' onClick="window.open('https://www.openstreetmap.org/?mlat=${latitude}&mlon=${longitude}&zoom=12#map=16/${latitude}/${longitude}', '_blank', 'popup=true,rel=noreferrer'); return false;" target="_blank">${name}</a>`;
+                let point = `<a href='https://www.openstreetmap.org/?mlat=${latitude}&mlon=${longitude}&zoom=12#map=16/${latitude}/${longitude}' onClick="window.open('https://www.openstreetmap.org/?mlat=${latitude}&mlon=${longitude}&zoom=12#map=16/${latitude}/${longitude}', '_blank', 'popup=true,rel=noreferrer'); return false;" target="_blank">${name}</a>`;
                 columns.station.push(point);
 
                 // Satellite Images
@@ -141,7 +141,8 @@ function generateChart(fileNameKey) {
             columnDefaults: {
                 sorting: {
                     sortable: true
-                }
+                },
+                useHTML: true
             },
 
             // https://api.highcharts.com/grid/#interfaces/Grid_Core_Options.Options#columns
